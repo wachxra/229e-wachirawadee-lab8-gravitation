aa9f7e9e74384aa0913630bfa5becddb66f858aa
wachxra <wachirawadee.junt@bumail.net>
2025-03-13 10:32:22 +0700

    Can be atrtract

--- 229e-wachirawadee-lab8/Assets/Scripts/GravityE.cs
+++ 229e-wachirawadee-lab8/Assets/Scripts/GravityE.cs
@@ -1,13 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityE : MonoBehaviour
 {
     Rigidbody rb;
     const float G = 0.006674f;
+    public static List<GravityE> gravityObjectList;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null )
+        {
+            gravityObjectList = new List<GravityE>();
+        }
+
+        gravityObjectList.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var objPlanets in gravityObjectList)
+        {
+            if (objPlanets != this)
+            {
+                Attract(objPlanets);
+            }
+        }
     }
 
     void Attract(GravityE other)
